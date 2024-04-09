f41b717b9cf9b4d947abd295ba66eb302710f5ed
xhWang00 <113479633+xhWang00@users.noreply.github.com>
2024-04-08 22:52:26 -0400

    Connects landmarks together.

--- unity3d/plotLandmarks.cs
+++ unity3d/plotLandmarks.cs
@@ -6,11 +6,28 @@ public class LandmarkPlotter : MonoBehaviour
 {
     public GameObject landmarkPrefab;
     public string apiUrl = "http://localhost:5000/landmarks";
-    public float updateInterval = 0.1f;
+    public float updateInterval = 0.01f;
 
     private void Start()
     {
         StartCoroutine(UpdateLandmarks());
+    }
+
+    private void connect_landmarks(GameObject[] objs, int start, int end)
+    {
+        var obj = new GameObject();
+        obj.tag = "Line";
+
+        var lineRender = obj.AddComponent<LineRenderer>();
+        lineRender.startWidth = 0.03f;
+        lineRender.endWidth = 0.03f;
+        lineRender.material.color = Color.white;
+
+        GameObject start_obj = objs[start];
+        GameObject end_obj = objs[end];
+
+        lineRender.SetPosition(0, start_obj.transform.position);
+        lineRender.SetPosition(1, end_obj.transform.position);
     }
 
     IEnumerator UpdateLandmarks()
@@ -28,8 +45,6 @@ public class LandmarkPlotter : MonoBehaviour
             {
                 string jsonResponse = www.downloadHandler.text;
                 LandmarkData[] landmarks = JsonHelper.FromJson<LandmarkData>(jsonResponse);
-                
-                Debug.Log(landmarks);
 
                 // Delete existing landmarks
                 GameObject[] existingLandmarks = GameObject.FindGameObjectsWithTag("Landmark");
@@ -37,13 +52,50 @@ public class LandmarkPlotter : MonoBehaviour
                 {
                     Destroy(landmark);
                 }
+                int i = 0;
+                GameObject[] objArr = new GameObject[21];
+                GameObject[] existingLines = GameObject.FindGameObjectsWithTag("Line");
+                foreach (GameObject line in existingLines)
+                {
+                    Destroy(line);
+                }
 
                 // Create new landmarks
                 foreach (LandmarkData landmarkData in landmarks)
                 {
                     Vector3 position = new Vector3(landmarkData.x, landmarkData.y, landmarkData.z);
-                    Instantiate(landmarkPrefab, position, Quaternion.identity);
+
+                    GameObject obj = Instantiate(landmarkPrefab, position, Quaternion.identity);
+                    objArr[i] = obj;
+                    i++;
                 }
+
+                connect_landmarks(objArr, 0, 17);
+                connect_landmarks(objArr, 0, 1);
+                connect_landmarks(objArr, 1, 2);
+                connect_landmarks(objArr, 2, 5);
+                connect_landmarks(objArr, 5, 9);
+                connect_landmarks(objArr, 9, 13);
+                connect_landmarks(objArr, 13, 17);
+
+                connect_landmarks(objArr, 2, 3);
+                connect_landmarks(objArr, 3, 4);
+
+                connect_landmarks(objArr, 5, 6);
+                connect_landmarks(objArr, 6, 7);
+                connect_landmarks(objArr, 7, 8);
+
+                connect_landmarks(objArr, 9, 10);
+                connect_landmarks(objArr, 10, 11);
+                connect_landmarks(objArr, 11, 12);
+
+                connect_landmarks(objArr, 13, 14);
+                connect_landmarks(objArr, 14, 15);
+                connect_landmarks(objArr, 15, 16);
+
+                connect_landmarks(objArr, 17, 18);
+                connect_landmarks(objArr, 18, 19);
+                connect_landmarks(objArr, 19, 20);
             }
 
             yield return new WaitForSeconds(updateInterval);
